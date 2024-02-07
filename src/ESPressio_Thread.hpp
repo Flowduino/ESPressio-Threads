7166640fa7caaf83af189ab48e4959757045e683
LK-Simon <lakraven@gmail.com>
2024-02-07 11:22:52 +0100

    - Fixing Thread

--- src/ESPressio_Thread.hpp
+++ src/ESPressio_Thread.hpp
@@ -20,7 +20,7 @@ namespace ESPressio {
             `Thread` is a class that represents a "standard" Thread in the system.
             It is a wrapper around the system's Thread API, designed to make them much easier to use.
         */
-        class Thread : public IThread, public Object<Thread> {
+        class Thread : public IThread, public Object<IThread> {
             private:
             // Type Definitions
                 
